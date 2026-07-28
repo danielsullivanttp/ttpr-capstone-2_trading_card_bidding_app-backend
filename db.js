3667445cebabf12765a8b60c8354e3d8b811f159
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-07-28 15:22:59 -0400

    seed.js: seeded my table with testData

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const {Sequelize} = require('sequelize');
 
-const db = new Sequelize('postgres://postgres:root@localhost:5432/TradingCard_app',{logging: false});
+const db = new Sequelize('postgres://postgres:root@localhost:5432/TradingCard',{logging: false});
 
 module.exports = db;
