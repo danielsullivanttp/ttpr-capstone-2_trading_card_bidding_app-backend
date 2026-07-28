72864ed768f75020b30454127b24b7e8f80fc6c2
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-07-28 14:09:53 -0400

    app.js: startApp() running on port 3000, Tradingcard.js: defined the table model

--- models/TradingCard.js
+++ models/TradingCard.js
@@ -1,2 +1,32 @@
-const {DataTypes} = require("sequelize");
-const db = require("../data/db");
+const {DataTypes, STRING} = require("sequelize");
+const db = require("../db");
+
+const TradingCard = db.define('TradingCard', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category: {
+        type: DataTypes.STRING,
+        allowNull: true    
+    },
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false,       
+    },
+    value: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    rare: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+});
+
+module.exports = TradingCard;
