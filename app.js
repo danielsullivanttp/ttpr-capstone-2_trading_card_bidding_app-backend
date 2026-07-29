b58e70a56889476c8beb30742236e60b2a3d5219
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-07-29 11:09:26 -0400

    routes/trading-cards.js: Set up Routes for Tradingcards

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const {db}  = require("./models/TradingCard");
-
+const TradingCardsRouter = require("./routes/trading-cards");
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.redirect("/Tradingcard")
+} )
 
 app.get('/health', (req, res) => {
     res.json({status: "ok"});
 });
 
+app.use("/TradingCard", TradingCardsRouter);
+
 async function startApp(){
     await db.sync();
     app.listen(PORT, () => {console.log("Server running on port 3000!!!")});
-db.close()
 }
 
 startApp();
