6be03afef10cf754a75e31aff1c768a1a57ac0f9
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-07-29 14:31:25 -0400

    Updated tradingCard GET all Router

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ttpr-capstone-2_trading_card_bidding_app-backend
+
+This will be an app that allows people to bid on collectable trading cards. It will have the option to create a new ticket/bid and have the verified value 
--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const {db}  = require("./models/TradingCard");
+const { db } = require("./models");
 const TradingCardsRouter = require("./routes/trading-cards");
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.redirect("/Tradingcard")
-} )
+  res.redirect("/Tradingcard");
+});
 
-app.get('/health', (req, res) => {
-    res.json({status: "ok"});
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
 });
 
 app.use("/TradingCard", TradingCardsRouter); // (Mounting router from "./routes/trading-cards") adds the /TradingCard prefix
 // ****Isn't repeated in routes/trading-cards.js***
 
-async function startApp(){
-    await db.sync();
-    app.listen(PORT, () => {console.log("Server running on port 3000!!!")});
+async function startApp() {
+  await db.sync();
+  app.listen(PORT, () => {
+    console.log("Server running on port 3000!!!");
+  });
 }
 
 startApp();
